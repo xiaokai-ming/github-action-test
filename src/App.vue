1318f2573b4250b7ad22ddfee3e5c047f58daff1
肖凯 <xiaokai@mininglamp.com>
2020-08-11 15:53:32 +0800

    feat: ceshi

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to My Vue.js App"/>
+    <HelloWorld msg="Welcome to My Vue.js Project"/>
   </div>
 </template>
 
